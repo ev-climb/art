7d12b31adcbc760996e6a3e2508863a2ae78d3df
Evgenii Evseev <ev-ev89@mail.ru>
2023-02-24 16:58:11 +0300

    Сделал, чтобы картины не повторялись

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,18 @@ import Loading from './Loading';
 const Home = ({ onClickMain, clicked, setClicked, selectedYear }) => {
   const API_URL = 'https://63f1c2684f17278c9a1961ec.mockapi.io/';
   const [arts, setArts] = React.useState([]);
+  const [allArts, setAllArts] = React.useState([]);
+  const [usedArts, setUsedArts] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+  const errorArt = {
+    title: 'Увы!',
+    artist: 'Ничего не найдено',
+    url: '/img/errorArt.jpg',
+    description:
+      'Попробуйте изменить настройки поиска. Кажется, с данными параметрами у нас пока ничего нет.',
+    year: ':-(',
+    years: '',
+  };
   async function fetchArt() {
     try {
       setLoading(true);
@@ -19,8 +30,31 @@ const Home = ({ onClickMain, clicked, setClicked, selectedYear }) => {
           }`,
         ),
       ]);
-      console.log(artsRes);
-      setArts(artsRes.data[Math.floor(Math.random() * artsRes.data.length)]);
+
+      if (allArts.length === 0) {
+        setAllArts(artsRes.data);
+      }
+
+      const availableArts = artsRes.data.filter(
+        (art) => !usedArts.some((usedArt) => usedArt.title === art.title),
+      );
+      console.log(usedArts, 'usedArts');
+      console.log(availableArts, 'availableArts');
+      console.log(allArts, 'allArts');
+
+      const randomArt = availableArts.length
+        ? availableArts[Math.floor(Math.random() * availableArts.length)]
+        : errorArt;
+
+      setUsedArts((prevUsedArts) => [...prevUsedArts, randomArt]);
+
+      setArts(randomArt);
+
+      if (availableArts.length === 0) {
+        setUsedArts([]);
+        setArts(errorArt);
+        setLoading(false);
+      }
     } catch (err) {
       alert('Ошибка запроса данных :(');
       console.log(err);
@@ -29,14 +63,34 @@ const Home = ({ onClickMain, clicked, setClicked, selectedYear }) => {
     }
   }
 
+  //     setAvailableArts(
+  //       artsRes.data.filter((art) => !usedArts.some((usedArt) => usedArt.title === art.title)),
+  //     );
+
+  //     console.log(usedArts, 'usedArts');
+  //     console.log(availableArts, 'availableArts');
+
+  //     const randomArt = availableArts.length
+  //       ? availableArts[Math.floor(Math.random() * availableArts.length)]
+  //       : errorArt;
+
+  //     setUsedArts((prevUsedArts) => [...prevUsedArts, randomArt]);
+
+  //     setArts(randomArt);
+  //   } catch (err) {
+  //     alert('Ошибка запроса данных :(');
+  //     console.log(err);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+
   React.useEffect(() => {
     fetchArt();
   }, []);
 
   async function onClickMain() {
-    {
-      clicked && (await fetchArt());
-    }
+    clicked && (await fetchArt());
     setClicked(!clicked);
   }
 
