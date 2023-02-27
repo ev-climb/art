77da4b149cc8827d893bf1198bdfa858726117d2
Evgenii Evseev <ev-ev89@mail.ru>
2023-02-27 16:01:38 +0300

    Заменил стили выпадающих списков

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,88 +1,3 @@
-// import React from 'react';
-// import '../styles.css';
-
-// const Settings = ({ selectedYear, setSelectedYear, selectedStyle, setSelectedStyle }) => {
-//   const handleSelectYear = (event) => {
-//     const selectedYearValue = event.target.value;
-//     localStorage.setItem('selectedYear', selectedYearValue);
-//   };
-
-//   const handleSelectStyle = (event) => {
-//     const selectedStyleValue = event.target.value;
-//     localStorage.setItem('selectedStyle', selectedStyleValue);
-//   };
-//   console.log(selectedStyle);
-//   return (
-//     <div className="settings">
-//       <div className="settingsImage">
-//         <ul>
-//           <li>
-//             <select
-//               id="year-select"
-//               value={selectedYear}
-//               onChange={(event) => {
-//                 setSelectedYear(event.target.value);
-//                 handleSelectYear(event);
-//               }}
-//             >
-//               <option value="All years">All years</option>
-//               <option value="&lt;1500">&lt; 1500</option>
-//               <option value="1500-1600">1500 - 1600</option>
-//               <option value="1600-1700">1600 - 1700</option>
-//               <option value="1700-1800">1700 - 1800</option>
-//               <option value="1800-1900">1800 - 1900</option>
-//               <option value="1900&lt;">1900 &lt;</option>
-//             </select>
-//           </li>
-//           <li>
-//             <select
-//               id="style-select"
-//               value={selectedStyle}
-//               onChange={(event) => {
-//                 setSelectedStyle(event.target.value);
-//                 handleSelectStyle(event);
-//               }}
-//             >
-//               <option value="All-styles">All styles</option>
-//               <option value="avant-garde">Avant-garde</option>
-//               <option value="baroque">Baroque</option>
-//               <option value="cubism">Cubism</option>
-//               <option value="expressionism">Expressionism</option>
-//               <option value="impressionism">Impressionism</option>
-//               <option value="modernism">Modernism</option>
-//               <option value="neo-pop">Neo-pop</option>
-//               <option value="pop-art">Pop-art</option>
-//               <option value="post-impressionism">Post-impressionism</option>
-//               <option value="postmodernism">Postmodernism</option>
-//               <option value="primitivism">Primitivism</option>
-//               <option value="renaissance">Renaissance</option>
-//               <option value="romanticism">Romanticism</option>
-//               <option value="street-art">Street-art</option>
-//               <option value="surrealism">Surrealism</option>
-//               <option value="suprematism">Suprematism</option>
-//             </select>
-//           </li>
-//           <li>
-//             <select id="artist-select">
-//               <option value="All-artist">All artist</option>
-//               <option value="leonardo-da-vinci">Leonardo da Vinci</option>
-//               <option value="vincent-van-gogh">Vincent van Gogh</option>
-//               <option value="pablo-picasso">Pablo Picasso</option>
-//               <option value="claude-monet">Claude Monet</option>
-//               <option value="salvador-dali">Salvador Dali</option>
-//               <option value="rembrandt-van-rijn">Rembrandt van Rijn</option>
-//               <option value="edvard-munch">Edvard Munch</option>
-//               <option value="jackson-pollock">Jackson Pollock</option>
-//             </select>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Settings;
-
 import React from 'react';
 import Select from 'react-select';
 import '../styles.css';
@@ -158,35 +73,42 @@ const Settings = ({
   const customStyles = {
     control: (provided) => ({
       ...provided,
-      backgroundColor: 'rgba(197,147,147,0.8)',
+      backgroundColor: 'rgba(0,0,0,0.4)',
       borderRadius: '8px',
-      height: '40px',
+      height: '20px',
+      width: '150px',
       cursor: 'pointer',
       color: '#fff',
-    }),
-    placeholder: (provided) => ({
-      ...provided,
-      color: '#fff',
-      fontWeight: 'bold',
+      ':hover': {
+        backgroundColor: 'rgba(0,0,0,0.7)',
+      },
     }),
     option: (provided, state) => ({
       ...provided,
-      color: state.isSelected ? '#fff' : '#333',
-      backgroundColor: state.isSelected ? '#007bff' : '#fff',
+      color: state.isSelected ? '#fff' : '#fff',
+      backgroundColor: state.isSelected ? '#007bff' : 'rgba(0,0,0,0.4)',
       ':hover': {
         backgroundColor: state.isSelected ? '#007bff' : '#f2f2f2',
         color: state.isSelected ? '#fff' : '#333',
       },
     }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#fff',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: 'rgba(0,0,0,0.4)',
+    }),
   };
 
   const handleSelectYear = (selectedOption) => {
-    setSelectedYear(selectedOption.value);
+    setSelectedYear(selectedOption.label);
     localStorage.setItem('selectedYear', selectedOption.value);
   };
 
   const handleSelectStyle = (selectedOption) => {
-    setSelectedStyle(selectedOption.value);
+    setSelectedStyle(selectedOption.label);
     localStorage.setItem('selectedStyle', selectedOption.value);
   };
 
