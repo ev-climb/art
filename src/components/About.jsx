fa20a27d2817675f0d64e2abee00fbbb8adb11ef
Evgenii Evseev <ev-ev89@mail.ru>
2023-02-27 18:34:26 +0300

    Добавил страницу About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import '../styles.css';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="aboutImage">
+      <div className="about">
+        <p>
+          Перед вами незаменимый помощник для всех любителей искусства, который поможет тренировать
+          «насмотренность» и освежить в памяти самые известные работы знаменитых художников разных
+          эпох и стилей.
+        </p>
+        <a href="mailto:arts.guru.info@gmail.com">WRITE TO US.....</a>
+      </div>
+    </div>
+  );
 };
 
 export default About;
