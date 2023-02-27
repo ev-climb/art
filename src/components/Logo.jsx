99aabe5e9d3ae3847c294bb51c2a757aa54c3fab
Evgenii Evseev <ev-ev89@mail.ru>
2023-02-27 18:59:35 +0300

    Добавил анимацию logo при клике по нему

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Logo = ({ clicked }) => {
+  const [onClickLogo, setOnClickLogo] = React.useState(false);
+  console.log(onClickLogo);
   return (
     <Link to="/" className="logo-link">
-      <div className="logo">
-        <div className={`logo-left ${clicked && 'logo-left-animation-on'}`}>
+      <div
+        className="logo"
+        onClick={() => {
+          setOnClickLogo(true);
+          setTimeout(() => setOnClickLogo(false), 1000);
+        }}
+      >
+        <div className={`logo-left ${clicked || onClickLogo ? 'logo-left-animation-on' : ''}`}>
           <span> ARTS</span>
         </div>
-        <div className={`logo-right ${clicked && 'logo-right-animation-on'}`}>GURU</div>
+        <div className={`logo-right ${clicked || onClickLogo ? 'logo-right-animation-on' : ''}`}>
+          GURU
+        </div>
       </div>
     </Link>
   );
